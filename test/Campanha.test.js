4df985acabf922cac43539aff7e3dc8d32ae99ca
Leo Fabricio <leofabr3@gmail.com>
2023-11-23 21:42:30 -0300

    still many errors while testing

--- test/Campanha.test.js
+++ test/Campanha.test.js
@@ -1,20 +1,67 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider()); // ganache.provider() is a provider that connects to the ganache network 
+const fs = require('fs-extra');
 
-const compiledFactory = require('../ethereum/build/CampanhaFactory.json');
-const compiledCampanha = require('../ethereum/build/Campanha.json');
+describe('Your Test Suite Description', () => {
+    let campanhaFactoryContract;
 
-let accounts;
-let factory;
-let campanhaAddress;
-let campanha;
+    before(() => {
+        const filePath = '../ethereum/build/CampanhaFactory.json';
+        try {
+            const data = fs.readFileSync(filePath, 'utf8');
+            campanhaFactoryContract = JSON.parse(data);
+        } catch (err) {
+            console.error('Error reading file:', err);
+        }
+    });
 
-beforeEach(async () => {
-    accounts = await web3.eth.getAccounts();
-
-    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-        .deploy({ data: compiledFactory.bytecode })
-        .send({ from: accounts[0], gas: '1000000' });
+    it('Your Test Case Description', () => {
+        assert.ok(campanhaFactoryContract); // Verify if the contract object is correctly loaded
+    });
 });
+
+// const compiledFactory = fs.readFileSync('../ethereum/build/CampanhaFactory.json', 'utf8');
+// const compiledCampanha = require('../ethereum/build/Campanha.json');
+
+// let accounts;
+// let factory;
+// let campanhaAddress;
+// let campanha;
+
+// fs.readFile('../ethereum/build/CampanhaFactory.json', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+//     const campanhaFactoryContract = JSON.parse(data);
+//     console.log(campanhaFactoryContract); // Verify if the contract object is correctly loaded
+// });
+
+
+// beforeEach(async () => {
+//     accounts = await web3.eth.getAccounts();
+
+//     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+//         .deploy({ data: compiledFactory.bytecode })
+//         .send({ from: accounts[0], gas: '1000000' });
+
+//     await factory.methods.createCampanha('100').send({
+//         from: accounts[0],
+//         gas: '1000000'
+//     });
+
+//     [campanhaAddress] = await factory.methods.getDeployedCampaigns().call();
+//     campanha = await new web3.eth.Contract(
+//         JSON.parse(compiledCampanha.interface),
+//         campanhaAddress
+//     );
+// });
+
+// describe('Campanhas', () => {
+//     it('deploys a factory and a campanha', () => {
+//         assert.ok(factory.options.address);
+//         assert.ok(campanha.options.address);
+//     });
+// });
