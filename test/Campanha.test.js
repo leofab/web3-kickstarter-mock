c118f8953e70f25d98d6ce4da4aec9f13646f583
Leo Fabricio <leofabr3@gmail.com>
2023-11-25 00:03:57 -0300

    tried git repository with code from 2 years ago but failed

--- test/Campanha.test.js
+++ test/Campanha.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
-const ganache = require('ganache');
-const { Web3 } = require('web3');
-const web3 = new Web3(ganache.provider()); // ganache.provider() is a provider that connects to the ganache network 
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
 const fs = require('fs-extra');
 
 let accounts;
@@ -9,69 +9,26 @@ let factory;
 let campanhaAddress;
 let campanha;
 
-describe('Tests if the contract is correctly loaded', () => {
-    let campanhaFactoryContract;
-    let campanhaContract;
+const compiledFactory = fs.readFileSync('/home/pc/Área\ de\ Trabalho/kickstart-mock/ethereum/build/CampanhaFactory.json', 'utf8');
+const compiledCampanha = fs.readFileSync('/home/pc/Área\ de\ Trabalho/kickstart-mock/ethereum/build/Campanha.json', 'utf8');
 
-    before(() => {
-        const filePath = '/home/pc/Área\ de\ Trabalho/kickstart-mock/ethereum/build/CampanhaFactory.json';
-        try {
-            const data = fs.readFileSync(filePath, 'utf8');
-            campanhaFactoryContract = JSON.parse(data);
-            // console.log(campanhaFactoryContract); // Verify if the contract object is correctly loaded  
-        } catch (err) {
-            console.error('Error reading file:', err);
-        }
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.abi))
+        .deploy({ data: '0x' + JSON.parse(compiledFactory).evm.bytecode.object })
+        .send({ from: accounts[0], gas: '1500000' });
+
+    await factory.methods.criarCampanha('100').send({ from: accounts[0], gas: '1500000' });
+
+    [campanhaAddress] = await factory.methods.getDeployedCampanhas().call();
+
+    campanha = await new web3.eth.Contract(compiledCampanha.abi, campanhaAddress);
+});
+
+describe('Campanhas', () => {
+    it('deploys a factory and a campanha', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campanha.options.address);
     });
-
-    before(() => {
-        const filePath = '/home/pc/Área\ de\ Trabalho/kickstart-mock/ethereum/build/Campanha.json';
-        try {
-            const data = fs.readFileSync(filePath, 'utf8');
-            campanhaContract = JSON.parse(data);
-            // console.log(campanhaContract); // Verify if the contract object is correctly loaded  
-        } catch (err) {
-            console.error('Error reading file:', err);
-        }
-    });
-
-    before(async () => {
-        accounts = await web3.eth.getAccounts();
-
-        try {
-            const dataFactory = fs.readFileSync('/home/pc/Área de Trabalho/kickstart-mock/ethereum/build/CampanhaFactory.json', 'utf8');
-            campanhaFactoryContract = JSON.parse(dataFactory);
-
-            factory = await new web3.eth.Contract(campanhaFactoryContract.abi)
-                .deploy({ data: '0x' + campanhaFactoryContract.evm.deployedBytecode.object })
-                .send({ from: accounts[0], gas: '1000000', gasPrice: '5000000000' });
-
-            const transactionReceipt = await factory.deployed();
-            console.log('Contract deployed at:', transactionReceipt.contractAddress);
-
-            await factory.methods.createCampanha('100').send({
-                from: accounts[0], gas: '1000000'
-            });
-            campanhaAddress = await factory.methods.getDeployedCampaigns().call({ from: accounts[0] });
-            campanha = await new web3.eth.Contract(campanhaContract.abi, campanhaAddress);
-            console.log(factory.options.address.object);
-        } catch (error) {
-            console.log(error);
-        }
-
-
-    });
-
-    it('Is file loaded correctly?', () => {
-        assert.ok(campanhaFactoryContract); // Verify if the contract object is correctly loaded
-        assert.ok(campanhaContract); // Verify if the contract object is correctly loaded
-    });
-    it('Is the interface loaded correctly?', () => {
-        assert.ok(campanhaFactoryContract.abi); // Verify if the contract object is correctly loaded
-        assert.ok(campanhaContract.abi);
-    });
-    // it('deploys a factory and a campanha', () => {
-    //     assert.ok(factory);
-    //     assert.ok(campanha);
-    // });
 });
