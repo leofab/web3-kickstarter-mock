7e661baddd7b465f863fe1e7facb42ad480de27c
Leo Fabricio <leofabr3@gmail.com>
2023-11-26 11:23:07 -0300

    hardhat.config.js configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.23",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.23",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./ethereum/contracts",
+    artifacts: "./ethereum/build",
+  },
 };
