62c414f20f1fcc6c9e06d699bb637d9ee82afd5c
Leo Fabricio <leofabr3@gmail.com>
2023-11-27 11:35:40 -0300

    install hdwalletproiver and final deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,25 +5,21 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require('web3');
 const { ethers } = require("hardhat");
 
+const provider = new HDWalletProvider('oak remain obtain task drip boy early wet segment sport fun source',
+    'https://sepolia.infura.io/v3/c0e92b8c927e4421bf01f62fb85487d4');
+
+const web3 = new Web3(provider, null, {transactionConfirmationBlocks: 1});
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+  const result = await ethers.deployContract("Campanha", [], {from: accounts[0]});
+  await result.waitForDeployment();
+  console.log("Contract deployed to:", result.deploymentTransaction.to);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
