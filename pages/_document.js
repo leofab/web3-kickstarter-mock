35b1447d47a1b03a5f062d1016263850c41f2b01
Leo Fabricio <leofabr3@gmail.com>
2023-11-28 09:31:33 -0300

    switching branches to refactor contract

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,12 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
                     />
-                    {/* Other meta tags, styles, etc. */}
+                    <link rel="stylesheet" type="text/css" href="css/semantic.min.css"/>
+                        <script type="text/javascript" src="js/jquery-3.0.0.min.js"></script>
+                        <script type="text/javascript" src="js/semantic.min.js"></script>
+
+                        <link rel="stylesheet" type="text/css" href="dist/semantic-ui-alerts.css"/>
+                            <script type="text/javascript" src="dist/semantic-ui-alerts.js"></script>
                 </Head>
                 <body>
                 <Main />
