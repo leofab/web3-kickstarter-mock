bd55e9e448f0f563b03b23d0fd9730dc55e881f7
Leo Fabricio <leofabr3@gmail.com>
2023-11-28 18:24:56 -0300

    setting props and state of requets/new

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,13 +1,86 @@
 import React, {Component} from 'react';
 import Header from '../../../components/Header';
+import {Form, Button, Message, Input} from 'semantic-ui-react';
+import Campaign from '../../../campaign';
+import web3 from '../../../web3';
+import {Router} from '../../../routes';
 
 class RequestNew extends Component {
+    state = {
+        value: '',
+        description: '',
+        recipient: '',
+        errorMessage: '',
+        loading: false
+    };
+
+    static async getInitialProps(props) {
+        const address = props.query.address;
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        console.log(address);
+        return {
+            minimumContribution: summary[0].toString(),
+            balance: summary[1].toString(),
+            requestsCount: summary[2].toString(),
+            approversCount: summary[3].toString(),
+            manager: summary[4],
+            address: address
+        };
+    }
+
+    onSubmit = async (e) => {
+        e.preventDefault();
+        const { address, manager } = this.props;
+        const { description, value, recipient } = this.state;
+        this.setState({ loading: true, errorMessage: '' })
+        try{
+            if(manager !== web3.eth.defaultAccount) {
+                throw new Error('Only the manager can create a request');
+            }
+            const accounts = await web3.eth.getAccounts();
+            const campaign = Campaign(address);
+            await campaign.methods.createRequest(description, web3.utils.toWei(value, 'ether'), recipient).send({
+                from: accounts[0]
+            });
+            Router.pushRoute(`/campaigns/${address}/requests`);
+        } catch (err) {
+            this.setState({ loading: false, errorMessage: err.message, value: '' });
+            console.log(err);
+        }finally {
+            this.setState({ loading: false, errorMessage: '', value: '' })
+        }
+    }
 
     render() {
         return (
             <Header>
-
+                <h1>Campanha {this.props.address}</h1>
             <h3>Request New</h3>
+                <Form>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event => this.setState({description: event.target.value})}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Value in Ether</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event => this.setState({value: event.target.value})}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Recipient</label>
+                        <Input
+                            value={this.state.recipient}
+                            onChange={event => this.setState({recipient: event.target.value})}
+                        />
+                    </Form.Field>
+                    <Button primary>Submit</Button>
+                </Form>
             </Header>
         );
     }
